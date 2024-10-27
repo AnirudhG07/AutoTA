8a04d97f8acd38ffa79faeeca20d1bc48feb3f70
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-10-27 12:08:18 +0530

    for proofjsonshorter, it works

--- src/gpt_structured.py
+++ src/gpt_structured.py
@@ -1,5 +1,7 @@
 import base64
+import json
 import os
+from itertools import groupby
 from os.path import join
 from pathlib import Path
 
@@ -22,6 +24,8 @@ Follow the below rules while extracting text from the image:
 4. The generated proof will be used to CHECK the correctness of the original proof, so DO NOT make corrections, add unmentioned reasonings complete proofs, only clean up the language.
 """
 
+PROOF_JSON_PROMPT = open(join(homedir, "src", "ProofJsonShorter.md")).read()
+
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
@@ -33,7 +37,7 @@ client = OpenAI(
 def image_solution(image_path: str):
     image_path = encode_image(image_path)
     
-    gpt_prompt = "Extract text with LaTeX from the given mathematics solution."
+    gpt_prompt = "Extract text with LaTeX from the given mathematics solution. GIVE ONLY THE PROOF."
 
     response = client.chat.completions.create(
         model="gpt-4o",
@@ -59,7 +63,7 @@ def image_solution(image_path: str):
 
     return response.choices[0].message.content
 
-def gpt_text_gen(prompt:str, task:str = "", model:str ="gpt-4o"):
+def gpt_response_gen(prompt:str, task:str = "", model:str ="gpt-4o"):
 
     messages = []
     if task != "":
@@ -74,7 +78,7 @@ def gpt_text_gen(prompt:str, task:str = "", model:str ="gpt-4o"):
             messages=messages,
         )
 
-    return str(response.choices[0].message)
+    return response.choices[0].message.content
 
 
 def solution_from_images(image_paths):
@@ -83,47 +87,63 @@ def solution_from_images(image_paths):
         response = image_solution(image_path)
         combined_text += str(response)
 
-    task =f"Your task is to rewrite the extracted text as a clean mathematical proof with full sentences, conjuctions etc. \n {ocr_rules}"
+    task =f"You are proficient in extracting Mathematical text from images. Your task is to rewrite the extracted text as a clean mathematical proof with full sentences, conjuctions etc. \n {ocr_rules}"
     prompt = f"Proof is: {combined_text}"
 
-    return gpt_text_gen(task, prompt)
+    return str(gpt_response_gen(task, prompt))
 
-def save_proof(path:str , text_proof:str, structured_proof, pid:str):
-    with open(join(path, f"{pid}.md"), "w") as f:
-        f.write(text_proof)
-    with open(join(path, f"{pid}.json"), "w") as f:
+def save_proof(path:str , text_proof:str, structured_proof, pid:str, thm:str):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    with open(join(path, f"{pid}_sol.md"), "w") as f:
+        f.write(f"## Theorem:\n {thm}\n\n## Proof:\n{text_proof}")
+
+    with open(join(path, f"{pid}_gpt_sol.json"), "w") as f:
         f.write(structured_proof)
     
-proof_json = open(join(homedir, "src", "ProofJsonShorter.md")).read()
 
 def structure_prompt(thm, pf):
-    return f"{proof_json}\n---\n\n## Theorem: {thm}\n\n## Proof: {pf}\n"
+    return f"{PROOF_JSON_PROMPT}\n---\n\n## Theorem: {thm}\n\n## Proof: {pf}\n"
+
+def gen_structure_proof(thm: str, pf: str):
+    response = gpt_response_gen(structure_prompt(thm, pf))
+    print(response)
+    if response is None:
+        return "No response from model while generating structured proof"
+    response_cleaned = response.strip("```json").strip("```")
+    
+    return json.dumps(json.loads(response_cleaned), indent=4)
 
 def structure_prompt_with_knowns(thm, pf, knowns):
-    return f"{proof_json}\n---\n\n## Theorem: {thm}\n\n## Proof: {pf}\n\n---\n\nThe following are known results that can be used without proof, even implicitly. DO NOT report the use of these results as errors or missing steps.\n\n## Known results: \n\n{knowns}\n"
+    return f"{PROOF_JSON_PROMPT}\n---\n\n## Theorem: {thm}\n\n## Proof: {pf}\n\n---\n\nThe following are known results that can be used without proof, even implicitly. DO NOT report the use of these results as errors or missing steps.\n\n## Known results: \n\n{knowns}\n"
 
 def truly_missing(knowns, s):
     prompt = f"The following are known results that can be used without proof, even implicitly.  \n\n## Known results: \n\n{knowns}\n\n---\n\nThe following was reported as a missing step in a proof:\n {s}\n\nDoes this result follow from the above known results? Answer 'yes' or 'no'."
-    return gpt_text_gen(prompt)
+    return gpt_response_gen(prompt)
 
 def gen_proof_from_dir(image_path):
-    for problem_dir in os.listdir(image_path):
-        previous_pid = ""
-        for prob in os.listdir(join(image_path, problem_dir)):
+    for problem_dir in os.listdir(image_path):  # problem_dir is Problem1_main
 
-            if prob == "theorem.md":
-                thm = open(join(image_path, problem_dir, prob)).read()
+        files = os.listdir(join(image_path, problem_dir))
+        grouped_files = {k: list(v) for k, v in groupby(sorted(files), key=lambda x: x.split("_")[0])}
 
-            elif prob.endswith(".png"):
-                cur_pid = prob.split(".")[0].split("_")[0]
+        prob_thm = open(join(image_path, problem_dir, "theorem.md")).read()
 
-                if previous_pid != cur_pid:
-                    previous_pid = cur_pid
-                    image_paths = [join(image_path, prob)]
-
-                else:
-                    previous_pid = cur_pid
-
-            else:
-                # Probably some other cache file or something
+        for st, pf_imgs in grouped_files.items():
+            if st == "theorem.md":
                 pass
+            else:
+                pf_imgs = [join(image_path, problem_dir, img) for img in pf_imgs]
+                text_proof = solution_from_images(pf_imgs)
+                print(text_proof)
+                print("*************")
+                structured_proof = gen_structure_proof(prob_thm, text_proof)
+                save_proof(join(results, "uma101_23_main", "selected_problems", problem_dir), text_proof, structured_proof, st, prob_thm)
+
+    # Temporarily breaking the loop after the first problem
+            break
+        break
+ 
+if __name__ == "__main__":
+    image_path = join(data, "uma101_23_main", "selected_problems")
+    gen_proof_from_dir(image_path)
