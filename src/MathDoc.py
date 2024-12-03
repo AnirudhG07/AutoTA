1e45d5af2dc4295127893161ea6acd6b8e3bbe90
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-12-03 16:27:50 +0530

    mathdoc touches complete. all done

--- src/MathDoc.py
+++ src/MathDoc.py
@@ -105,7 +105,7 @@ root_child.append(define)
 ## Assert
 
 claim = MathDocTree("claim", "The mathematical claim being asserted, NOT INCLUDING proofs, justifications or results used. The claim should be purely a logical statement which is the *consequence* obtained.", give_json="string")
-proof_method = MathDocTree("proof_method", "The method used to prove the claim. This could be a direct proof, proof by contradiction, proof by induction, etc. this should be a single phrase or a fairly simple sentence; if a longer justification is needed break the step into smaller steps. If the method is deduction from a result, use the 'deduced_using' field", optional = True, give_json="string")
+proof_method = MathDocTree("proof_method", "The method used to prove the claim. This could be a direct proof, proof by contradiction, proof by induction, etc. this should be a single phrase or a fairly simple sentence; if a longer justification is needed break the step into smaller steps. If the method is deduction from a result, use the 'deduced_using' field.", optional = True, give_json="string")
 
 
 instantiation = MathDocTree("instantiation", "Specific numbers, functions etc to which a known result is applied. For example, if we apply uniqueness of prime factorisation to `42` write `{'result_used' : 'uniqueness of prime factorization', 'instantiation': '42'}`.")
@@ -122,7 +122,7 @@ deduced_from_results = MathDocTree("deduced_from_results", node_sequence_txt("de
 ### Calculate
 
 inline = MathDocTree("inline_calculation", "A simple calculation or computation written as a single line.", give_json="string")
-calculation_step = MathDocTree("step", "A step, typically an equality or inequality, in a calculation or computation.", give_json="string")
+calculation_step = MathDocTree("calculation_step", "A step, typically an equality or inequality, in a calculation or computation.", give_json="string")
 calculation_sequence = MathDocTree("calculation_sequence", node_sequence_txt("calculation_step"), [calculation_step]) 
 
 calculate = MathDocTree("calculate", "An equation, inequality, short calculation etc.", [inline, calculation_sequence], optional = True, give_json="object", key_value_str=["inline_calculation", "calculation_sequence"])
@@ -132,10 +132,10 @@ missing = MathDocTree("missing", "A  problem that need to be solved or results t
 missing_proofs = MathDocTree("missing_proofs", node_sequence_txt("missing"), [missing], optional=True)
 
 ### Errors
-errors = MathDocTree("errors", "An error in a proof or calculation. Report only actual errors, with missing steps reported in the 'missing' field.", give_json = "string")
-error = MathDocTree("error", node_sequence_txt("error"), children = [errors], optional=True)
+error = MathDocTree("error", "An error in a proof or calculation. Report only actual errors, with missing steps reported in the 'missing' field.", give_json = "string")
+errors = MathDocTree("errors", node_sequence_txt("error"), children = [error], optional=True)
 
-assert_children = [claim, proof_method, deduced_from_results, calculate, missing_proofs, error]
+assert_children = [claim, proof_method, deduced_from_results, calculate, missing_proofs, errors]
 
 assert_type = MathDocTree("assert", "A mathematical statement whose proof is a straightforward consequence of given and known results following some method.", assert_children, give_json = "object", post_text= subkeys_posttext) 
 root_child.append(assert_type)
@@ -151,16 +151,16 @@ proof = MathDocTree("proof", "A proof of a lemma, theorem or claim, having the s
 ref = MathDocTree("ref", "A reference where the result has been previously proved.", optional = True, give_json="string")
 cite = MathDocTree("cite", "A citation of a result from the mathematical literature which gives the proof.", optional= True, give_json="string")
 
-theorem = MathDocTree("theorem", "A mathematical theorem, with a list of hypotheses and a conclusion.", children = [statement, hypothesis, conclusion, proved, proof, ref, cite, missing_proofs, error])
+theorem = MathDocTree("theorem", "A mathematical theorem, with a list of hypotheses and a conclusion.", children = [hypothesis, conclusion, proved, name_field, proof, ref, cite, missing_proofs, errors], give_json= "object", post_text= subkeys_posttext)
 root_child.append(theorem)
 
 ## Problem
 
-problem_statement = MathDocTree("problem_statement", "The statement of the problem.", give_json="string")
+problem_statement = MathDocTree("statement", "The statement of the problem.", give_json="string")
 solved = MathDocTree("solved", "Whether the problem has been solved", give_json="boolean")
 answer = MathDocTree("answer", "The answer to the problem.", optional = True, give_json="string")
 
-problem = MathDocTree("problem", "A mathematical problem, with a statement and an answer.", children = [problem_statement, solved, answer, proof, missing_proofs, error], give_json="object", post_text= subkeys_posttext)
+problem = MathDocTree("problem", "A mathematical problem, with a statement and an answer.", children = [problem_statement, solved, answer, proof, missing_proofs, errors], give_json="object", post_text= subkeys_posttext)
 root_child.append(problem)
 
 ## Cases
@@ -172,13 +172,13 @@ on = MathDocTree("on", "The variable or expression on which the cases are being 
 #### Case
 condition = MathDocTree("condition", "The case condition or pattern; for induction one of 'base' or 'induction-step'; for a side of an 'iff' statement write the claim being proved (i.e., the statement `P => Q` or `Q => P`).", give_json="string")
 
-case = MathDocTree("case", "A case in a proof by cases or proof by induction.", children = [condition, proof, missing_proofs, error], give_json="object", post_text= subkeys_posttext)
+case = MathDocTree("case", "A case in a proof by cases or proof by induction.", children = [condition, proof, missing_proofs, errors], give_json="object", post_text= subkeys_posttext)
 
 proof_cases = MathDocTree("proof_cases", node_sequence_txt("case"), [case], optional = True)
 
 exhaustiveness = MathDocTree("exhaustiveness", "Proof that the cases are exhaustive, similar to (the value for) 'math_document'.", optional = True, give_json="list", key_value_str = md_blobnames)
 
-cases = MathDocTree("cases", "A proof by cases or proof by induction, with a list of cases.", children = [split_kind, on, proof_cases, exhaustiveness, missing_proofs, error], give_json="object", post_text= subkeys_posttext)
+cases = MathDocTree("cases", "A proof by cases or proof by induction, with a list of cases.", children = [split_kind, on, proof_cases, exhaustiveness, missing_proofs, errors], give_json="object", post_text= subkeys_posttext)
 root_child.append(cases)
 
 ## Induction
@@ -191,7 +191,7 @@ root_child.append(induction)
 assumption = MathDocTree("assumption", "The assumption being made for the contradiction.", give_json="string")
 assumption_proof = MathDocTree("proof", "The proof of the contradiction given the assumption.", give_json="list", key_value_str = md_blobnames)
 
-contradiction = MathDocTree("contradiction", "A proof by contradiction, with an assumption and a proof of the contradiction.", children = [assumption, assumption_proof, missing_proofs, error], give_json="object", post_text= subkeys_posttext)
+contradiction = MathDocTree("contradiction", "A proof by contradiction, with an assumption and a proof of the contradiction.", children = [assumption, assumption_proof, missing_proofs, errors], give_json="object", post_text= subkeys_posttext)
 root_child.append(contradiction)
 
 ## Calculate
@@ -199,7 +199,7 @@ root_child.append(calculate)
 
 ## Conclude
 conlusion_claim = MathDocTree("claim", "The conclusion of the proof.", give_json="string")
-conclude = MathDocTree("conclude", "conclude a claim obtained from the steps so far. this is typically the final statement of a proof giving the conclusion of the theorem.", children = [ conlusion_claim, missing_proofs, error], give_json="object", post_text= subkeys_posttext)
+conclude = MathDocTree("conclude", "conclude a claim obtained from the steps so far. this is typically the final statement of a proof giving the conclusion of the theorem.", children = [ conlusion_claim, missing_proofs, errors], give_json="object", post_text= subkeys_posttext)
 root_child.append(conclude)
 
 ## Remarks
